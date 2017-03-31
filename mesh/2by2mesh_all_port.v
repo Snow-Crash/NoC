2f207fefba0dff5f01f00830a1be4baae3039485
Snow-Crash <hfang02@syr.edu>
2017-03-31 00:16:24 -0400

    add parameter
    add parameter NUM_NURNS, NUM_AXON, NURN_CNT_BIT_WIDTH,
    AXON_CNT_BIT_WIDTH.
    every cell can have different number of neurons

--- mesh/2by2mesh_all_port.v
+++ mesh/2by2mesh_all_port.v
@@ -25,7 +25,8 @@ wire [1:0] full_row1, full_row2, full_column1, full_column2;
 wire [1:0] write_row1, write_row2, write_column1, write_column2;
 
 
-neuron_cell #(.X_ID("1"), .Y_ID("1")) cell_1_1(.clk(clk), .rt_clk(rt_clk), .rst_n(rst_n), .rt_reset(rt_reset),
+neuron_cell #(.X_ID("1"), .Y_ID("1"), .NUM_NURNS(8), .NUM_AXONS(8), .NURN_CNT_BIT_WIDTH(3), .AXON_CNT_BIT_WIDTH(3))
+cell_1_1(.clk(clk), .rt_clk(rt_clk), .rst_n(rst_n), .rt_reset(rt_reset),
 .clk_north(rt_clk), .clk_south(rt_clk), .clk_east(rt_clk), .clk_west(rt_clk),
 .north_in(n_in_1_1), .south_in(data_wire_column1[3:0]), .east_in(data_wire_row1[3:0]), .west_in(w_in_1_1),
 .north_neighbor_full(n_n_full_1_1), .south_neighbor_full(full_column1[0]), .east_neighbor_full(full_row1[1]), .west_neighbor_full(w_n_full_1_1),
@@ -34,7 +35,8 @@ neuron_cell #(.X_ID("1"), .Y_ID("1")) cell_1_1(.clk(clk), .rt_clk(rt_clk), .rst_
 .write_req_north(north_w_1_1), .write_req_south(write_column1[1]), .write_req_east(write_row1[1]), .write_req_west(west_w_1_1),
 .write_en_north(n_en_1_1), .write_en_south(write_column1[0]), .write_en_east(write_row1[0]), .write_en_west(w_en_1_1), .start(start));
 
-neuron_cell #(.X_ID("1"), .Y_ID("2")) cell_1_2(.clk(clk), .rt_clk(rt_clk), .rst_n(rst_n), .rt_reset(rt_reset),
+neuron_cell #(.X_ID("1"), .Y_ID("2"), .NUM_NURNS(2), .NUM_AXONS(2), .NURN_CNT_BIT_WIDTH(1), .AXON_CNT_BIT_WIDTH(1)) 
+cell_1_2(.clk(clk), .rt_clk(rt_clk), .rst_n(rst_n), .rt_reset(rt_reset),
 .clk_north(rt_clk), .clk_south(rt_clk), .clk_east(rt_clk), .clk_west(rt_clk),
 .north_in(n_in_1_2), .south_in(data_wire_column2[3:0]), .east_in(e_in_1_2), .west_in(data_wire_row1[7:4]),
 .north_neighbor_full(n_n_full_1_2), .south_neighbor_full(full_column2[0]), .east_neighbor_full(e_n_full_1_2), .west_neighbor_full(full_row1[1]),
@@ -43,7 +45,8 @@ neuron_cell #(.X_ID("1"), .Y_ID("2")) cell_1_2(.clk(clk), .rt_clk(rt_clk), .rst_
 .write_req_north(north_w_1_2), .write_req_south(write_column2[1]), .write_req_east(east_w_1_2), .write_req_west(write_row1[0]),
 .write_en_north(n_en_1_2), .write_en_south(write_column2[0]), .write_en_east(e_en_1_2), .write_en_west(write_row1[1]), .start(start));
 
-neuron_cell #(.X_ID("2"), .Y_ID("1")) cell_2_1(.clk(clk), .rt_clk(rt_clk), .rst_n(rst_n), .rt_reset(rt_reset),
+neuron_cell #(.X_ID("2"), .Y_ID("1"), .NUM_NURNS(2), .NUM_AXONS(2), .NURN_CNT_BIT_WIDTH(1), .AXON_CNT_BIT_WIDTH(1)) 
+cell_2_1(.clk(clk), .rt_clk(rt_clk), .rst_n(rst_n), .rt_reset(rt_reset),
 .clk_north(rt_clk), .clk_south(rt_clk), .clk_east(rt_clk), .clk_west(rt_clk),
 .north_in(data_wire_column1[7:4]), .south_in(s_in_2_1), .east_in(data_wire_row2[3:0]), .west_in(w_in_2_1),
 .north_neighbor_full(full_column1[1]), .south_neighbor_full(s_n_full_2_1), .east_neighbor_full(full_row2[1]), .west_neighbor_full(w_n_full_2_1),
@@ -52,7 +55,8 @@ neuron_cell #(.X_ID("2"), .Y_ID("1")) cell_2_1(.clk(clk), .rt_clk(rt_clk), .rst_
 .write_req_north(write_column1[0]), .write_req_south(south_w_2_1), .write_req_east(write_row2[1]), .write_req_west(west_w_2_1),
 .write_en_north(write_column1[1]), .write_en_south(s_en_2_1), .write_en_east(write_row2[0]), .write_en_west(w_en_2_1), .start(start));
 
-neuron_cell #(.X_ID("2"), .Y_ID("2")) cell_2_2(.clk(clk), .rt_clk(rt_clk), .rst_n(rst_n), .rt_reset(rt_reset),
+neuron_cell #(.X_ID("2"), .Y_ID("2"), .NUM_NURNS(2), .NUM_AXONS(2), .NURN_CNT_BIT_WIDTH(1), .AXON_CNT_BIT_WIDTH(1)) 
+cell_2_2(.clk(clk), .rt_clk(rt_clk), .rst_n(rst_n), .rt_reset(rt_reset),
 .clk_north(rt_clk), .clk_south(rt_clk), .clk_east(rt_clk), .clk_west(rt_clk),
 .north_in(data_wire_column2[7:4]), .south_in(s_in_2_2), .east_in(e_in_2_2), .west_in(data_wire_row2[7:4]),
 .north_neighbor_full(full_column2[1]), .south_neighbor_full(s_n_full_2_2), .east_neighbor_full(e_n_full_2_2), .west_neighbor_full(full_row2[0]),

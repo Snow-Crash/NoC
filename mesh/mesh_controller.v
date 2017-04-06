df589524631ea63d0af75a3f749beea9c972c316
Snow-Crash <hfang02@syr.edu>
2017-04-06 15:23:56 -0400

    multiple changes
    change size of several counters,  add comment. add a new output, which
    is a bit-wise xor of the output of result_ram, to prevent result_ram
    removed by quartus. move the location of two initialize blocks.

--- mesh/mesh_controller.v
+++ mesh/mesh_controller.v
@@ -3,9 +3,9 @@
 //          packet, another stores how many spike are input into network.
 //          connect with a router's local port.
 //2017.4.6  add a new ram to collect result
+//          change packet_number_rom size from 6 to 8
 
-
-module mesh_controller(neu_clk, rst_n, rt_clk, rt_reset, start, spike_packet, write_req, packet_in, write_enable, receive_full);
+module mesh_controller(neu_clk, rst_n, rt_clk, rt_reset, start, spike_packet, write_req, packet_in, write_enable, receive_full, result_output);
 
 
 input neu_clk, rst_n, rt_clk, rt_reset, write_enable;
@@ -13,7 +13,7 @@ input [3:0] packet_in;
 output reg start;
 output [31:0] spike_packet;
 output reg write_req;
-output receive_full;
+output receive_full, result_output;
 
 parameter packet_size = 32;
 parameter spike_number = 32;
@@ -26,22 +26,22 @@ localparam init = 3'd1;
 localparam send = 3'd2;
 localparam wait4clk = 3'd3;
 
-reg [ADDR_WIDTH - 1:0] spike_rom_address;
-reg inc_spike_rom_address;
+reg [ADDR_WIDTH - 1:0] packet_rom_address;
+reg inc_packet_rom_address;
 //packet rom, stores all packet
-reg [31:0] spike_rom[2**ADDR_WIDTH - 1:0];//
-reg [31:0] spike_rom_out;
+reg [31:0] packet_rom[2**ADDR_WIDTH - 1:0];//
+reg [31:0] packet_rom_out;
 // store how many packets are read in one step
-reg [5:0] packet_number_rom[2**ADDR_WIDTH - 1:0];
-reg [5:0] packet_number_rom_out;
-reg [9:0] packet_counter;
+reg [7:0] packet_number_rom[2**ADDR_WIDTH - 1:0];
+reg [7:0] packet_number_rom_out;
+reg [7:0] packet_counter;
 reg clear_packet_counter, inc_packet_counter, inc_step;
-reg [9:0] step_counter;
-reg [9:0] neu_cycle_counter;
+reg [7:0] step_counter;
+reg [7:0] neu_cycle_counter;
 
 reg clear_neu_cycle_counter, inc_neu_cycle_counter;
 
-wire [31:0] result_packet, result_output;
+wire [31:0] result_packet;
 wire receive_fifo_empty;
 wire read_receive_fifo;
 reg write_result;
@@ -59,11 +59,26 @@ spikebuf receive_fifo (
 	);
 
 
-assign read_receive_fifo = ~ receive_fifo_empty;
-
 reg [31:0] result [2**ADDR_WIDTH-1:0];
 reg [ADDR_WIDTH - 1:0] result_address_reg;
 reg [ADDR_WIDTH - 1:0] result_address;
+
+
+assign read_receive_fifo = ~ receive_fifo_empty;
+
+//initialize packet_number_rom. packet_number_rom store each step how many packets are read
+
+initial
+    begin
+		$readmemh("../data1_1/packet_number_mif.txt", packet_number_rom);
+	end
+
+//initialize packet_rom; packet_rom contains all the spike packets
+initial
+    begin
+		$readmemh("../data1_1/spike_mif.txt", packet_rom);
+	end
+
 
 always @(posedge neu_clk or negedge rst_n)
     if(rst_n == 1'b0)
@@ -90,30 +105,19 @@ always @ (posedge neu_clk)
 		result_address_reg <= result_address;
 	end
 
-assign result_output = result[result_address];
+assign result_output = ^result[result_address];
 
-
-
-//initialize spike_rom; spike_rom contains all the spike packets
-initial
-    begin
-		$readmemh("../data1_1/spike_mif.txt", spike_rom);
-	end
-//spike_rom
+//packet_rom
 always @ (posedge neu_clk or negedge rst_n)
     begin
         if(rst_n == 1'b0)
-            spike_rom_out <= 0;
+            packet_rom_out <= 0;
         else
-		    spike_rom_out <= spike_rom[spike_rom_address];
+		    packet_rom_out <= packet_rom[packet_rom_address];
 	end
-assign spike_packet = spike_rom_out;
+assign spike_packet = packet_rom_out;
 
-//initialize packet_number_rom. packet_number_rom store each step how many packets are read
-initial
-    begin
-		$readmemh("../data1_1/packet_number_mif.txt", packet_number_rom);
-	end
+
 //packet_number_rom
 always @ (posedge neu_clk or negedge rst_n)
     begin
@@ -158,11 +162,11 @@ always @(posedge neu_clk or negedge rst_n)
     begin
         if (rst_n == 1'b0)
             fourclk_counter <= 0;
-        if(clear_fourclk_counter == 1'b1)
+        else if(clear_fourclk_counter == 1'b1)
             fourclk_counter <= 0;
         else if(inc_fourclk_counter)
             fourclk_counter <= fourclk_counter + 1;
-        else if (fourclk_counter == 8)
+        else
             fourclk_counter <= fourclk_counter;
     end
 
@@ -170,11 +174,11 @@ always @(posedge neu_clk or negedge rst_n)
 always @(posedge neu_clk or negedge rst_n)
     begin
         if(rst_n == 1'b0)
-            spike_rom_address = 0;
-        else if (inc_spike_rom_address)
-            spike_rom_address <= spike_rom_address + 1;
+            packet_rom_address <= 0;
+        else if (inc_packet_rom_address)
+            packet_rom_address <= packet_rom_address + 1;
         else
-            spike_rom_address <= spike_rom_address;
+            packet_rom_address <= packet_rom_address;
     end
 
 //fsm
@@ -204,7 +208,7 @@ always @(*)
                 begin
                         next_state = wait4clk;
                 end
-            wait4clk:
+            wait4clk://wait four clock, avoid boundary router's fifo is full. because no mechanism to detect and solve this problem
                 begin
                     if (fourclk_counter < 4)
                         next_state = wait4clk;
@@ -221,7 +225,7 @@ always @(*)
                         next_state = send;
                 end
         default:
-                next_state <= init;
+                next_state = init;
         endcase
 end
 
@@ -236,7 +240,7 @@ always @(*)
                     clear_neu_cycle_counter = 1'b1;
                     inc_packet_counter = 1'b0;
                     clear_packet_counter = 1'b1;
-                    inc_spike_rom_address = 1'b0;
+                    inc_packet_rom_address = 1'b0;
                     inc_step = 1'b0;
                     write_req = 1'b0;
                 end
@@ -249,7 +253,7 @@ always @(*)
                     clear_neu_cycle_counter = 1'b0;
                     inc_packet_counter = 1'b1;
                     clear_packet_counter = 1'b0;
-                    inc_spike_rom_address = 1'b1;
+                    inc_packet_rom_address = 1'b1;
                     inc_step = 1'b0;
                 end
             wait4clk:
@@ -259,7 +263,7 @@ always @(*)
                     clear_neu_cycle_counter = 1'b0;
                     clear_packet_counter = 1'b0;
                     inc_packet_counter = 1'b0;
-                    inc_spike_rom_address = 1'b0;
+                    inc_packet_rom_address = 1'b0;
                     inc_step = 1'b0;
                     if (fourclk_counter == 4) begin
                         clear_fourclk_counter = 1'b1;
@@ -275,7 +279,7 @@ always @(*)
                     inc_fourclk_counter = 1'b0;
                     
                     inc_packet_counter = 1'b0;
-                    inc_spike_rom_address = 1'b0;
+                    inc_packet_rom_address = 1'b0;
                     clear_neu_cycle_counter = 1'b0;
                     if (neu_cycle_counter == step_cycle) begin
                         inc_neu_cycle_counter = 1'b0;
@@ -295,7 +299,7 @@ always @(*)
                     inc_fourclk_counter = 1'b0;
                     clear_packet_counter = 1'b0;
                     inc_packet_counter = 1'b0;
-                    inc_spike_rom_address = 1'b0;
+                    inc_packet_rom_address = 1'b0;
                     inc_neu_cycle_counter = 1'b0;
                     clear_neu_cycle_counter = 1'b0;
                     inc_step = 1'b0;
@@ -322,7 +326,5 @@ always @(*)
         start = 1'b1;
     else
         start = 1'b0;
-
-
         
 endmodule

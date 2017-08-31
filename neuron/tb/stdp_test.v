11e1d2d8b66a351ecfaabe07ff05758284c5a3b7
Snow-Crash <hfang02@syr.edu>
2017-08-31 17:20:57 -0400

    change testbench
    change testbench to test  learing function of core.

--- neuron/tb/stdp_test.v
+++ neuron/tb/stdp_test.v
@@ -5,24 +5,27 @@ module stdp_test();
 
 parameter packet_size = 32;
 parameter flit_size = 4;
-parameter NUM_NURNS = 2;
-parameter NUM_AXONS = 2;
-parameter NURN_CNT_BIT_WIDTH = 1;
-parameter AXON_CNT_BIT_WIDTH = 1;
+parameter NUM_NURNS = 4;
+parameter NUM_AXONS = 4;
+parameter NURN_CNT_BIT_WIDTH = 2;
+parameter AXON_CNT_BIT_WIDTH = 2;
 parameter X_ID = "1";
 parameter Y_ID = "1";
 parameter SYNTH_PATH = "D:/code/synth/data";
-parameter SIM_PATH =  "D:/code/data";
+parameter SIM_PATH =  "D:/code/learn_test_work_dir/data";
 
 reg clk, rst_n;
-wire [31:0] SpikePacket;
 wire outSpike;
-wire [3:0] local_packet_out;
 reg start;
 
-wire [NUM_AXONS - 1:0] spike;
+reg [NUM_AXONS - 1:0] spike;
 
-Neuron uut (.clk(clk), .rst_n(rst_n), .SpikePacket(SpikePacket), .outSpike(outSpike),. start(start), .inSpike(spike));
+Neuron #(.NUM_NURNS(NUM_NURNS), 
+	.NUM_AXONS(NUM_AXONS), 
+	.SIM_PATH(SIM_PATH), 
+	.NURN_CNT_BIT_WIDTH(NURN_CNT_BIT_WIDTH), 
+	.AXON_CNT_BIT_WIDTH(AXON_CNT_BIT_WIDTH)) 
+	uut (.clk(clk), .rst_n(rst_n), .SpikePacket(SpikePacket), .outSpike(outSpike),. start(start), .inSpike(spike));
 
 
 	//cpu clock
@@ -41,33 +44,35 @@ initial
 		#(`tpd_clk*2);
 		rst_n = 1'b1;
 
-
+		spike = 4'b1111;
 		#((`tpd_clk*2)*2);
 		@(posedge clk); start = 1'b1; @(posedge clk); start = 1'b0;
 		
-		#((`tpd_clk*2)*30);//interval between two start signal
+		#((`tpd_clk*2)*100);//interval between two start signal
 
-
+		spike = 4'b0010;
 		#((`tpd_clk*2)*2);
 		@(posedge clk); start = 1'b1; @(posedge clk); start = 1'b0;
 
-		#((`tpd_clk*2)*30);
+		#((`tpd_clk*2)*100);
 
+		spike = 4'b1100;
         #((`tpd_clk*2)*2);
 		@(posedge clk); start = 1'b1; @(posedge clk); start = 1'b0;
 
-		#((`tpd_clk*2)*30);
+		#((`tpd_clk*2)*100);
 
-
+		spike = 4'b0100;
         #((`tpd_clk*2)*2);
 		@(posedge clk); start = 1'b1; @(posedge clk); start = 1'b0;
 
-		#((`tpd_clk*2)*30);
+		#((`tpd_clk*2)*100);
 
+		spike = 4'b0101;
         #((`tpd_clk*2)*2);
 		@(posedge clk); start = 1'b1; @(posedge clk); start = 1'b0;
 
-		#((`tpd_clk*2)*30);
+		#((`tpd_clk*2)*100);
 
 
 		$stop;

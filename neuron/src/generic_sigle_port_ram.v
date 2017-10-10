559f96c41c00a42f674971b40c48381d184cadc5
Snow-Crash <hfang02@syr.edu>
2017-10-10 10:59:09 -0400

    new module
    single port memory.

---
+++ neuron/src/generic_sigle_port_ram.v
@@ -0,0 +1,46 @@
+//https://www.altera.com/support/support-resources/design-examples/design-software/verilog/ver-single-port-ram.html
+`timescale 1ns/100ps
+`define SIM_MEM_INIT
+//`define QUARTUS_SYN_INIT
+
+module generic_single_port_ram (clk, addr, data_in, data_out, write_enable, read_enable);
+
+    parameter DATA_WIDTH = 16;
+    parameter ADDRESS_WIDTH = 8;
+    parameter SIM_FILE_PATH = "D:/code/data";
+	parameter INIT_FILE_PATH = "";
+
+    input clk, write_enable, read_enable;
+    input [DATA_WIDTH-1:0] data_in, data_out;
+    input [ADDRESS_WIDTH-1:0] addr;
+
+
+	reg [ADDRESS_WIDTH-1:0] addr_reg;
+
+    `ifdef SIM_MEM_INIT
+        reg [DATA_WIDTH-1:0] mem [(1<<ADDRESS_WIDTH) -1:0];
+	    reg [100*8:1] file_name;
+	    initial 
+            begin
+		    file_name = SIM_FILE_PATH;				$readmemh (file_name, mem);
+	        end
+    `else
+        `ifdef QUARTUS_SYN_INIT
+	        (* ram_init_file = INIT_FILE_PATH *) reg [DATA_WIDTH-1:0] mem [(1<<ADDRESS_WIDTH) -1:0];
+	    `else
+		    reg [DATA_WIDTH-1:0] mem [(1<<ADDRESS_WIDTH) -1:0];
+	        `endif
+    `endif
+ 
+    always @ (posedge clk)
+        begin
+            if (write_enable)
+                mem[addr] <= data_in;
+            if (read_enable)
+                addr_reg <= addr;
+            
+        end
+	assign data_out = mem[addr_reg];
+
+ 
+endmodule
